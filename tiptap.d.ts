49d7c9d62a6445835c8ed196a1d32220ec57aaaf
poya <damdadodam_@naver.com>
2026-01-06 17:31:37 +0900

    Type Add

--- tiptap.d.ts
+++ tiptap.d.ts
@@ -1,12 +1,27 @@
-import "@tiptap/core";
-
+import '@tiptap/core'
 declare module '@tiptap/core' {
-    interface Commands<ReturnType> {
+        interface Commands<ReturnType> {
         color: {
             setColor: (color: string) => ReturnType
         }
         highlight: {
             setHighlight: (options: { color: string }) => ReturnType
+        }
+        table: {
+            insertTable: (options: { rows: number; cols: number; withHeaderRow?: boolean }) => ReturnType
+            deleteTable: () => ReturnType
+            addRowBefore: () => ReturnType
+            addRowAfter: () => ReturnType
+            deleteRow: () => ReturnType
+            addColumnBefore: () => ReturnType
+            addColumnAfter: () => ReturnType
+            deleteColumn: () => ReturnType
+            mergeCells: () => ReturnType
+            splitCell: () => ReturnType
+            toggleHeaderRow: () => ReturnType
+            toggleHeaderColumn: () => ReturnType
+            toggleHeaderCell: () => ReturnType
+            fixTables: () => ReturnType
         }
     }
 }
